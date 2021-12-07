2feb8c16fa2f2be1390e9123ff3f2ffe5e1ff14d
Mehrdad Hojati <mehrdad.hojati@outlook.com>
2021-12-07 15:08:43 +0330

    bug fixed

--- common/models/country.model.ts
+++ common/models/country.model.ts
@@ -3,6 +3,6 @@ import { CorpService } from "../../tools/services/corp.service";
 export class Country {
     private data: string[];
     constructor(private corpService: CorpService) {
-        this.data = corpService.CallAPI();
+        //this.data = corpService.CallAPI();
     }
 }
